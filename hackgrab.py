1ae9b7c550fb976e99951e0feefb7fe790a17d6b
Glenn C. Lasher Jr <git@kc2idf.com>
2025-06-02 20:30:41 -0400

    Added functionality to future "does-all" library, not yet utilized.  Corrected an issue with the outed-detection logic in launch.

--- hackgrab.py
+++ hackgrab.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import os
+import time
+import re
 
 class Loggable (object):
     def __init__(self, debug=False, verbose=False):
@@ -55,6 +57,56 @@ class Channel (Loggable):
 
         return good
 
+    def datesort(self):
+        relevantre = re.compile ("^(.*\])\.(mp4|webm|mkv)$")
+        new_filename = 'new.m3u'
+
+        new_content = []
+        new_update  = False
+
+        if os.path.isfile(new_filename):
+            with open(new_filename, 'r') as infile:
+                new_content = infile.read().strip().split('\n')
+
+        for filename in os.listdir():
+            relevantmatch = relevantre.match(filename)
+            if relevantmatch:
+                stub    = relevantmatch.group(1)
+                stats   = os.lstat(filename)
+                date    = time.gmtime(stats.st_mtime)
+                dateyr  = "%04d" % (date.tm_year,)
+                datemn  = "%02d" % (date.tm_mon,)
+                datedt  = "%02d" % (date.tm_mday,)
+
+                target  = os.path.join(dateyr, datemn, datedt)
+                desc    = "%s.description" % (relevantmatch.group(1),)
+                moveto  = os.path.join(target, filename)
+
+                if self.verbose:
+                    print ("For", filename, ":")
+                    print ("    We will create", target)
+                    print ("    And move the file to", moveto)
+
+                new_content.append(moveto)
+                new_update = True
+        
+                os.makedirs(target, exist_ok = True)
+                os.rename(filename, moveto)
+
+                if os.path.isfile(desc):
+                    moveto  = os.path.join(target, desc)
+
+                    if self.verbose:
+                        print ("    And move", desc)
+                        print ("    To", moveto)
+                        print ("", flush=True)
+
+                    os.rename(desc, moveto)
+            
+        if new_update:
+            with open(new_filename, "w") as outfile:
+                outfile.write("\n".join(new_content[-10:]) + '\n')
+    
     def destroy(self):
         #TODO: This.
         pass
